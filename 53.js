e666bed88d53686bcd3640d45fb518d202a4d847
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:45:28 -0300

    53

---
+++ 53.js
@@ -0,0 +1,35 @@
+/*53. Crea una variable llamada `diaSemana` y asígnale el nombre de un día de la semana.
+Utiliza una declaración `switch` para mostrar un mensaje que indique si es un "Día
+laboral" o "Fin de semana" en función del valor de `diaSemana`.*/
+
+var diaSemana = "domingo";
+
+switch (diaSemana) {
+  case "lunes":
+    console.log("Dia laboral");
+    break;
+
+  case "martes":
+    console.log("Dia laboral");
+    break;
+
+  case "miercoles":
+    console.log("Dia laboral");
+    break;
+
+  case "jueves":
+    console.log("Dia laboral");
+    break;
+
+  case "viernes":
+    console.log("Dia laboral");
+    break;
+
+  case "sabado":
+    console.log("Fin de semana");
+    break;
+
+  case "domingo":
+    console.log("Fin de semana");
+    break;
+}
