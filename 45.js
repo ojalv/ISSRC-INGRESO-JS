deaaafeb2a9b5d0ddd41f6544d8291dcb4f46c7a
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 14:53:17 -0300

    45

---
+++ 45.js
@@ -0,0 +1,11 @@
+/*45. De ne una variable llamada `numero1` y otra llamada `numero2`. Realiza una
+operación aritmética (suma, resta, multiplicación o división) con estas variables y muestra
+el resultado en la consola.*/
+
+var numero1, numero2;
+
+numero1 = 10;
+
+numero2 = 15;
+
+console.log(`suma: ${numero1} + ${numero2} \nresultado: ${numero1 + numero2}`);
