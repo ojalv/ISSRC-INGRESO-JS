b624f7371d94dee43d1638f10a4fbd70c2d23afe
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:09:54 -0300

    48

---
+++ 48.js
@@ -0,0 +1,8 @@
+/*48. Crea una variable llamada `radio` y asígnale el radio de un círculo. Calcula el área del
+círculo utilizando la fórmula `A = π * r^2` y muestra el resultado en la consola.*/
+
+var radio;
+
+radio = 15;
+
+console.log(`Area del circulo = ${(Math.PI * (radio**2)).toFixed(2)}cm`);
