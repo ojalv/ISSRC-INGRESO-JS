0f50f857b82c7f95294ff6211813d161e08a6d80
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:15:28 -0300

    49

---
+++ 49.js
@@ -0,0 +1,13 @@
+/*49. crea una variable llamada `num1` y otra llamada `num2`. Escribe una expresión que
+veri que si `num1` es mayor que `num2` y muestra el resultado (true o false) en la
+consola.*/
+
+var num1, num2;
+
+num1 = 2;
+
+num2 = 1;
+
+expresion = num1 > num2;
+
+console.log(`num1 > num2: ${expresion}`);
