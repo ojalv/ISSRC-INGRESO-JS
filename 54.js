80bb6633b34f5c52bc5b5c606223cab3e29c35f2
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:54:34 -0300

    54

---
+++ 54.js
@@ -0,0 +1,15 @@
+/*54. De ne dos variables, `numero1` y `numero2`, y realiza una comparación para
+determinar cuál de los dos números es mayor. Luego, muestra el número mayor en la
+consola.*/
+
+var numero1, numero2;
+
+numero1 = 20;
+numero2 = 155;
+
+if (numero1 > numero2) {
+  console.log(`${numero1} es mayor que ${numero2}`);
+}
+if (numero1 < numero2) {
+  console.log(`${numero2} es mayor que ${numero1}`);
+}
