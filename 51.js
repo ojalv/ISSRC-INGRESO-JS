cebf898b9e747ea6b1245e0e3ebfcc607951085c
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:33:20 -0300

    51

---
+++ 51.js
@@ -0,0 +1,11 @@
+/*51. Crea una variable llamada `edad` y asigna un valor numérico. Escribe una condición
+que veri que si la persona es mayor de 18 años y muestra un mensaje en la consola,
+como "Eres mayor de edad", si se cumple la condición*/
+
+var edad = 16;
+
+if (edad > 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("No eres mayor de edad");
+}
