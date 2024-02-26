c7511c73cf8d76bae01ee3ad7de697292759f9f2
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 14:48:37 -0300

    43

---
+++ 43.js
@@ -0,0 +1,13 @@
+/*43. Crea una variable llamada `esEstudiante` y asígnale `true` si eres estudiante o `false`
+si no lo eres. Luego, muestra un mensaje que diga "Eres estudiante" o "No eres
+estudiante" en la consola según el valor de la variable.*/
+
+var esEstudiante;
+
+esEstudiante = true;
+
+if (esEstudiante) {
+    console.log(`Eres estudiante`);
+} else {
+    console.log(`No eres estudiante`);
+}
