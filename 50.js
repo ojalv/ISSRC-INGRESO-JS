25189980b7fc6194cb849e14054911c3041c07b2
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:17:58 -0300

    50

---
+++ 50.js
@@ -0,0 +1,10 @@
+/*50. Crea una variable llamada `cadena1` y otra llamada `cadena2`. Concatena estas dos
+cadenas utilizando el operador `+` y muestra la cadena resultante en la consola.*/
+
+var cadena1, cadena2;
+
+cadena1 = "Las rosas son rojas ";
+
+cadena2 = "Las violetas son azules ";
+
+console.log(cadena1 + cadena2);
