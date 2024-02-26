e9bc4f98c18a28b198b58134c40261e2b4fda7c2
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 14:58:12 -0300

    46

---
+++ 46.js
@@ -0,0 +1,7 @@
+/*46. Crea una variable llamada `temperaturaCelsius` y asígnale un valor en grados
+Celsius. Convierte esta temperatura a grados Fahrenheit utilizando la fórmula `F = (C *
+9/5) + 32` y muestra el resultado en la consola.*/
+
+var temperaturaCelsius = 24;
+
+console.log(`${temperaturaCelsius}°C = ${(temperaturaCelsius * 9/5) + 32}°F`);
