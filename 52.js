e09cd6914680cca1d385e0ea0def666ed6378c4f
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:38:51 -0300

    52

---
+++ 52.js
@@ -0,0 +1,12 @@
+/*52. De ne una variable llamada `puntuacion` y asigna un valor numérico entre 0 y 100.
+Utiliza una estructura condicional `if...else` para mostrar un mensaje en la consola que
+indique si la puntuación es "Aprobada" (si es mayor o igual a 60) o "Reprobada" (si es
+menor a 60).*/
+var puntuacion = 70;
+
+if (puntuacion >= 60) {
+  console.log("Aprobada");
+}
+if (puntuacion < 60) {
+  console.log("Reprobada");
+}
