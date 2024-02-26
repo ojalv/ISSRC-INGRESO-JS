5a780708a518964d33cba65a2fa045f03a740c27
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 14:45:28 -0300

    42

---
+++ 42.js
@@ -0,0 +1,8 @@
+/*42. De ne una variable llamada `edad` y asígnale tu edad. Luego, muestra un mensaje
+que diga "Tienes X años" en la consola, reemplazando X con el valor de la variable.*/
+
+var edad;
+
+edad = 24;
+
+console.log(`Tienes ${edad} años`);
