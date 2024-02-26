d8850ce4f714e2b3a1942bfcafc2b7f1553f3632
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 15:01:52 -0300

    47

---
+++ 47.js
@@ -0,0 +1,10 @@
+/*47. Crea una variable llamada `precioProducto` y otra llamada `cantidadComprada`.
+Calcula el costo total de la compra multiplicando estas variables y muestra el resultado en
+la consola.*/
+
+var precioProducto, cantidadComprada;
+
+precioProducto = 10000;
+cantidadComprada = 4;
+
+console.log(`Costo total de la compra: $${precioProducto*cantidadComprada}ARS`);
