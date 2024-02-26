dafefe9d8469b2d2842b0cbe080b2a59d37f090b
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-26 14:50:17 -0300

    44

---
+++ 44.js
@@ -0,0 +1,8 @@
+/*44. Crea una variable llamada `fruta` y asígnale el nombre de tu fruta favorita. Luego,
+muestra un mensaje que diga "Mi fruta favorita es [nombre de la fruta]" en la consola.*/
+
+var fruta;
+
+fruta = "durazno";
+
+console.log(`Mi fruta favorita es ${fruta}`);
