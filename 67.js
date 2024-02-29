945f6c25885605383ad78dde79621275596db959
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-02-29 15:52:25 -0300

    67

---
+++ 67.js
@@ -0,0 +1,14 @@
+/*67- Escribe una función llamada calcularPotencia que tome dos números como
+argumento, base y exponente, y retorne el resultado de elevar la base al exponente.*/
+
+function calcularPotencia(base, exponente) {
+  return base ** exponente;
+}
+
+
+console.log(calcularPotencia(2,2))
+console.log(calcularPotencia(2,3))
+console.log(calcularPotencia(2,4))
+console.log(calcularPotencia(3,1))
+console.log(calcularPotencia(3,2))
+console.log(calcularPotencia(3,3))
