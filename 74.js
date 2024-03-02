e06253e1a44007cac1ad3b3af323fd38af371725
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-02 03:45:10 -0300

    74

---
+++ 74.js
@@ -0,0 +1,9 @@
+/*74. De ne una variable `contador` con un valor inicial de 1. Luego, utiliza un bucle
+`do...while` para mostrar los números del 1 al 5 en la consola.*/
+
+let contador = 0
+
+do {
+    contador++
+    console.log(contador);
+} while (contador < 5);
