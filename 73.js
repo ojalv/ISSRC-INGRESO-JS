bb9d92423a6ddeecc52d7bc973e16da41cb7cb11
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-02 03:41:29 -0300

    73

---
+++ 73.js
@@ -0,0 +1,8 @@
+/*73. Crea un arreglo de nombres de frutas. Utiliza un bucle `for...of` para recorrer el arreglo
+y mostrar cada nombre de fruta en la consola.*/
+
+let frutas = ["manzana", "banana", "pera", "kiwi"]
+
+for (const fruta of frutas) {
+    console.log(fruta);
+}
