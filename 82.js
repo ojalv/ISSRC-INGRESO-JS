67ab5c4d8c037961f052493bc04ce44aa96dfc3b
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-05 15:42:26 -0300

    82

---
+++ 82.js
@@ -0,0 +1,8 @@
+/*82. Escribe una función llamada `sumar` que tome dos argumentos y devuelva la suma de
+esos dos números.*/
+
+function sumar(a, b) {
+  return a + b;
+}
+
+console.log(sumar(11,2));//13
