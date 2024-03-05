0669831aed7fb02564dd707c1ac9bef2be49650b
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-05 15:25:09 -0300

    79

---
+++ 79.js
@@ -0,0 +1,9 @@
+/*79. Escribe una función que imprima la tabla de multiplicar de un número especí co
+utilizando un bucle `for`.*/
+
+let n = 3;
+console.log(`Tabla del ${n}:`);
+
+for (let i = 1; i <= 10; i++) {
+  console.log(n * i);
+}
