469990c985f05ba320a7afe26da459f02395f381
alvarito1999 <oj.alv99@gmail.com>
2024-03-11 01:06:39 -0300

    123

---
+++ 123/123.js
@@ -0,0 +1,7 @@
+/*123. Crea un array con nombres de frutas. Utiliza un bucle `for` para recorrer el array y
+mostrar cada nombre de fruta en la consola.*/
+let frutas = ["banana", "manzana", "naranja", "pera"];
+
+frutas.forEach((fruta) => {
+  console.log(fruta);
+});
