f08a32421c07fc5953a3eeeb1946c0f164b22a0b
alvarito1999 <oj.alv99@gmail.com>
2024-03-11 00:46:21 -0300

    121

---
+++ 121/121.js
@@ -0,0 +1,6 @@
+/*121. Crea un array llamado `números` con números del 1 al 5 y muestra su contenido en
+la consola. */
+const numeros = [1, 2, 3, 4, 5];
+numeros.forEach((numero) => {
+  console.log(numero);
+});
