78c9be79387bb8d07b2c62a743710ac2a017662b
alvarito1999 <oj.alv99@gmail.com>
2024-03-11 18:55:25 -0300

    125

---
+++ 125/125.js
@@ -0,0 +1,7 @@
+/*125. Crea un array de números y utiliza el método `push` para agregar un nuevo número
+al final del array.*/
+
+let array = [1, 2, 5, 4, 8, 3, 5, 5];
+console.log(`Array original ${array}`);
+array.push(666);
+console.log(`Array actualizado ${array}`);
