e9e827b956cc78d048f3a89dfb521dfcda6ee3c8
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-14 15:21:07 -0300

    131

---
+++ 131.js
@@ -0,0 +1,10 @@
+/*131. Crea un objeto JSON que represente información de un estudiante, incluyendo
+`nombre`, `edad`, y `curso`. Muestra este objeto en la consola.*/
+
+const estudiante = {
+  "nombre": "Hector",
+  "edad": 23,
+  "curso": "A"
+}
+
+console.log(estudiante);
