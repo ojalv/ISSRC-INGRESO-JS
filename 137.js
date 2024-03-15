6edce595e7a6d6610bc9cf80cb3930de1d2213d9
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-15 18:55:33 -0300

    137

---
+++ 137.js
@@ -0,0 +1,12 @@
+/*137. Crea un objeto JSON que represente información de una película, incluyendo `título`,
+`director`, y `año`. Luego, muestra cada propiedad en la consola.*/
+
+const pelicula = {
+  "nombre":"El secreto de sus ojos",
+  "director":"Juan José Campanella",
+  "año": 2009
+}
+
+console.log(pelicula.nombre);
+console.log(pelicula.director);
+console.log(pelicula.año);
