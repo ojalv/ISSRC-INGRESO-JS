09e0023fd36ca88479fdee5caa7245f51f2ceb33
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2024-03-20 15:49:07 -0300

    [maquetacion]

---
+++ 143/143.js
@@ -0,0 +1,4 @@
+/*143. Formulario de Registro:
+Diseña un formulario de registro en HTML con campos como nombre, correo electrónico y
+contraseña. Utiliza JavaScript para validar los campos y mostrar mensajes de error. Aplica
+estilos CSS para resaltar los campos incorrectos.*/
